4fb35df287cb937033f721c2fa903b3da7cc73ec
Jose Francisco Carneiro de Sousa <jfrancisco.mail@gmail.com>
2022-12-28 16:41:19 +0400

    sensor toggling

--- code.py
+++ code.py
@@ -5,11 +5,7 @@ import simpleio
 import adafruit_lsm6ds.lsm6ds33
 import adafruit_apds9960.apds9960
 from adafruit_hid.mouse import Mouse
-import adafruit_ble
-from adafruit_ble.advertising import Advertisement
-from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
 from adafruit_ble.services.standard.hid import HIDService
-from adafruit_ble.services.standard.device_info import DeviceInfoService
 
 i2c = board.I2C()
 
@@ -23,14 +19,22 @@ left_click = digitalio.DigitalInOut(board.BUTTON_A)
 left_click.direction = digitalio.Direction.INPUT
 left_click.pull = digitalio.Pull.UP
 
+sensor_btn = digitalio.DigitalInOut(board.BUTTON_B)
+sensor_btn.direction = digitalio.Direction.INPUT
+sensor_btn.pull = digitalio.Pull.UP
+
+prev_state = sensor_btn.value
+
 mouse_min = -9
 
 mouse_max = 9
 
 step = (mouse_max - mouse_min) / 20.0
 
+
 def mouse_steps(axis):
     return round((axis - mouse_min) / step)
+
 
 clock = 0
 
@@ -38,51 +42,33 @@ distance = 245
 
 hid = HIDService()
 
-device_info = DeviceInfoService(
-    software_revision=adafruit_ble.__version__,
-    manufacturer="Adafruit")
-
-advertisement = ProvideServicesAdvertisement(hid)
-
-advertisement.appearance = 961
-
-scan_response = Advertisement()
-
-scan_response.complete_name = "CP HID"
-
-ble = adafruit_ble.BLERadio()
-
-if not ble.connected:
-    print("Ready to connect")
-
-else:
-    print("already connected")
-    print(ble.connections)
-
 mouse = Mouse(hid.devices)
 
 while True:
-    while not ble.connected:
-        pass
-    while ble.connected:
-        x,y,z = accel.acceleration
+    cur_state = prev_state
+    if cur_state != prev_state:
+        print("sensor on")
 
-        horizontal_mov = simpleio.map_range(mouse_steps(x), 1.0,20.0,-15.0, 15.0)
-        vertical_mov = simpleio.map_range(mouse_steps(y), 20.0,1.0,-15.0,15.0)
-        scroll_dir = simpleio.map_range(vertical_mov, -15.0,15.0,3.0,-3.0)
+        x, y, z = accel.acceleration
+
+        horizontal_mov = simpleio.map_range(mouse_steps(x), 1.0, 20.0, -15.0, 15.0)
+        vertical_mov = simpleio.map_range(mouse_steps(y), 20.0, 1.0, -15.0, 15.0)
+        scroll_dir = simpleio.map_range(vertical_mov, -15.0, 15.0, 3.0, -3.0)
 
         if prox.proximity > distance:
-                mouse.move(wheel=int(scroll_dir))
+            mouse.move(wheel=int(scroll_dir))
         else:
-                mouse.move(x=int(horizontal_mov))
-                mouse.move(y=int(vertical_mov))
+            mouse.move(x=int(horizontal_mov))
+            mouse.move(y=int(vertical_mov))
 
         if not left_click.value:
             mouse.click(Mouse.LEFT_BUTTON)
             time.sleep(0.2)
-            
+
             if (clock + 2) < time.monotonic():
                 print("x", mouse_steps(x))
                 print("y", mouse_steps(y))
                 clock = time.monotonic()
-    ble.start_advertising(advertisement)
+    else:
+        print("sensor off")
+    prev_state = cur_state

a35d7a5d5d4e951d21b77fa32d22b94007f4a643
Jose Francisco Carneiro de Sousa <jfrancisco.mail@gmail.com>
2023-02-01 02:42:25 +0000

    Add import_data which to read csv file to pass for training.

--- calibration/calibration.py
+++ calibration/calibration.py
@@ -4,6 +4,7 @@ import digitalio
 import simpleio
 import adafruit_lsm6ds.lsm6ds33
 from helpers import csv_helpers
+import gc
 
 
 class Calibration:
@@ -12,26 +13,15 @@ class Calibration:
     def __init__(self):
         self.i2c = board.I2C()
         self.accel = adafruit_lsm6ds.lsm6ds33.LSM6DS33(self.i2c)
-        self.mouse_min = -9
-        self.mouse_max = 9
-        self.step = (self.mouse_max - self.mouse_min) / 20.0
-
-    def mouse_steps(self, axis):
-        return round((axis - self.mouse_min) / self.step)
 
     def calibrate(self):
-        x_movements = []
-        y_movements = []
-
-        step_counter = 0
-
-        calibrate_btn = digitalio.DigitalInOut(board.BUTTON_A)
+        calibrate_btn = digitalio.DigitalInOut(board.BUTTON_B)
         calibrate_btn.direction = digitalio.Direction.INPUT
         calibrate_btn.pull = digitalio.Pull.UP
         calibrate_btn_last_touch_val = False
         calibrate_btn_toggle_val = False
 
-        cancel_calibration_btn = digitalio.DigitalInOut(board.BUTTON_B)
+        cancel_calibration_btn = digitalio.DigitalInOut(board.BUTTON_A)
         cancel_calibration_btn.direction = digitalio.Direction.INPUT
         cancel_calibration_btn.pull = digitalio.Pull.UP
         cancel_calibration_btn_last_touch_val = False
@@ -44,31 +34,32 @@ class Calibration:
             if calibrate_btn_curr_state != calibrate_btn_last_touch_val:
                 if not calibrate_btn_curr_state:
                     self.calibration()
-
+                    gc.collect()
             if cancel_calibration_btn_curr_state != cancel_calibration_btn_last_touch_val:
                 if not cancel_calibration_btn_curr_state:
                     cancel_calibration_btn_toggle_val = not cancel_calibration_btn_toggle_val
                     cancel_calibration_btn.deinit()
                     calibrate_btn.deinit()
                     cancel_calibration_btn = None
-                    print("CALIBRATION CANCELLED")
+                    gc.collect()
                     break
             cancel_calibration_btn_last_touch_val = cancel_calibration_btn_curr_state
             calibrate_btn_last_touch_val = calibrate_btn_curr_state
 
     def calibration(self):
-        print("MOVE SIDE TO SIDE")
+        print("SIDE TO SIDE")
         start_time = time.monotonic()
         movement_data = []
         while (time.monotonic() - start_time) < 5:
             movement = self.accel.acceleration
             movement_data.append(movement + ("horizontal",))
-            time.sleep(0.1)
+            time.sleep(0.15)
         print("UP AND DOWN")
         start_time = time.monotonic()
         while (time.monotonic() - start_time) < 5:
             movement = self.accel.acceleration
             movement_data.append(movement + ("vertical",))
-            time.sleep(0.1)
-        print("SAVING")
+            time.sleep(0.15)
         self.csv_helpers.save_calibration_data(movement_data)
+        gc.collect()
+        print(self.csv_helpers.import_data().pop())
--- helpers/csv_helpers.py
+++ helpers/csv_helpers.py
@@ -1,13 +1,21 @@
 import circuitpython_csv as csv
 from ulab import numpy as np
-
+import time
+import gc
 class CsvHelpers():
     def __init__(self) -> None:
         pass
     def save_calibration_data(self,data):
+        gc.collect()
         with open("movement_data.csv", mode="w", encoding="utf-8") as file:
             writer = csv.writer(file)
-            writer.writerow(['x','y','z','movement_type'])
             writer.writerows(data)
-        print("DONE")
     
+    def import_data(self):
+        dataset = []
+        with open("movement_data.csv", mode="r") as file:
+            reader = csv.reader(file)
+            for line in reader:
+                dataset.append(line)
+                time.sleep(0.1)
+        return dataset
